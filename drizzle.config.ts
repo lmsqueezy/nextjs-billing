7974e172b2f6cd86739887e1b11646fa78111b1a
franklin <etrobot@outlook.com>
2024-05-16 00:06:17 +0800

    update drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,9 +5,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  driver: "pg",
+  dialect:"postgresql",
   dbCredentials: {
-    connectionString: process.env.POSTGRES_URL!,
+    url: process.env.POSTGRES_URL!,
   },
   verbose: true,
   strict: true,
