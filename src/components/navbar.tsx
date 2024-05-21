165544edb434557bc5dbbee312142cc8d1f88460
franklin <etrobot@outlook.com>
2024-05-21 23:42:49 +0800

    change navbar bg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 export default function Navbar() {
     return(
-    <div className="md:sticky md:top-0 md:z-50 md:flex md:items-center md:justify-between md:w-full md:h-12 md:p-2 md:shrink-0 md:bg-gradient-to-b md:from-background/10 md:via-background/50 md:to-background/80 md:backdrop-blur-xl fixed bottom-0 z-50 w-full bg-gradient-to-t from-background/10 via-background/50 to-background/80 p-2 backdrop-blur-xl flex items-center justify-between">
+    <div className="md:sticky md:top-0 md:z-50 md:flex md:items-center md:justify-between md:w-full md:h-12 md:p-2 md:shrink-0 fixed bottom-0 z-50 w-full  p-2 flex items-center justify-between bg-background">
     <ThemeToggle />
     <nav className='max-w-xs mx-auto flex gap-4'>
         <Link href='/'>HOME</Link>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import type { Config } from "tailwindcss"
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
