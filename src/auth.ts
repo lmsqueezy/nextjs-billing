d2f6fc38d0aba565428af71f8cdd7fbf4e847f91
Branko Conjic <brankoconjic@gmail.com>
2024-08-17 13:13:52 +0200

    Upgrade DrizzleAdapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,14 @@
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
-import { db } from "./db/schema";
+import { accounts, db, sessions, users } from "./db/schema";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+  }),
   providers: [GitHub],
   basePath: "/api/auth",
   pages: {
