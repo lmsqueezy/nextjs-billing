fa711081a1b8b04d2fb4be6788a8b90c69aad6f5
Branko Conjic <brankoconjic@gmail.com>
2024-08-17 13:13:16 +0200

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,10 +5,8 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.POSTGRES_URL!,
-  },
+  dialect: "postgresql",
+  dbCredentials: { url: process.env.POSTGRES_URL! },
   verbose: true,
   strict: true,
 });
