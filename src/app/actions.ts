739a0ddc851247cda87c82ed12cb9e3e02f883bb
Branko Conjic <brankoconjic@gmail.com>
2024-08-19 22:24:37 +0200

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -167,15 +167,11 @@ export async function syncPlans() {
 
   // Helper function to add a variant to the productVariants array and sync it with the database.
   async function _addVariant(variant: NewPlan) {
-    console.log(`Syncing variant ${variant.name} with the database...`);
-
     // Sync the variant with the plan in the database.
     await db
       .insert(plans)
       .values(variant)
       .onConflictDoUpdate({ target: plans.variantId, set: variant });
-
-    console.log(`${variant.name} synced with the database...`);
 
     productVariants.push(variant);
   }
@@ -274,8 +270,6 @@ export async function storeWebhookEvent(
     throw new Error("POSTGRES_URL is not set");
   }
 
-  console.log("Storing webhook event in the database...", eventName);
-
   const id = crypto.randomInt(100000000, 1000000000);
 
   const returnedValue = await db
@@ -298,14 +292,10 @@ export async function storeWebhookEvent(
 export async function processWebhookEvent(webhookEvent: NewWebhookEvent) {
   configureLemonSqueezy();
 
-  console.log(`Processing webhook event #${webhookEvent.id}...`);
-
   const dbwebhookEvent = await db
     .select()
     .from(webhookEvents)
     .where(eq(webhookEvents.id, webhookEvent.id));
-
-  console.log({ dbwebhookEvent });
 
   if (dbwebhookEvent.length < 1) {
     throw new Error(
@@ -318,8 +308,6 @@ export async function processWebhookEvent(webhookEvent: NewWebhookEvent) {
       "Missing required WEBHOOK_URL env variable. Please, set it in your .env file.",
     );
   }
-
-  console.log("Processing webhook event...", webhookEvent.eventName);
 
   let processingError = "";
   const eventBody = webhookEvent.body;
